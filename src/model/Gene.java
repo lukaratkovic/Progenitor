c2a8b995ecfdf5a70c264a4aed28b913693fbaea
lukaratkovic <luka.ratkovic09@gmail.com>
2023-06-07 11:08:56 +0200

    Added Javadoc for Gene and ValueGene classes

--- src/model/Gene.java
+++ src/model/Gene.java
@@ -1,18 +1,46 @@
 package model;
 
+/**
+ * Base implementation of a gene.
+ * @param <T> Generic type for the value of the gene
+ */
 public abstract class Gene<T> {
+    /**
+     * Value of the gene.
+     */
     public T value;
-    public Gene(){}
+
+    /**
+     * Abstract method for mutating the value of a Gene.
+     * @return mutated Gene
+     */
     public abstract Gene mutate();
 
+
+    /**
+     * Abstract method for returning a new instance of a gene with equal parameters.
+     * @return
+     */
     public abstract Gene getInstance();
 
+    /**
+     * Abstract method for creating a new instance of a gene with equal parameters and value.
+     * @return
+     */
     public abstract Gene clone();
 
+    /**
+     * Getter for the value of a Gene.
+     * @return value of the gene
+     */
     public T getValue() {
         return value;
     }
 
+    /**
+     * Returns a string representation of a Gene, containing its value
+     * @return a string representation of a Gene
+     */
     @Override
     public String toString() {
         return "Gene{" +
--- src/model/ValueGene.java
+++ src/model/ValueGene.java
@@ -1,5 +1,7 @@
 package model;
 
-import exceptions.ValueOutOfBoundsException;
-
+/**
+ * Base implementation of a value gene
+ * @param <T> Generic Comparable type for the value of the gene
+ */
 public abstract class ValueGene <T extends Comparable> extends Gene<T>{}
